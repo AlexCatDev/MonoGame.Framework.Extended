638dbdfada02579701377d7772601a1c8e30e41a
Alex <glovy0@hotmail.com>
2017-10-17 15:32:56 +0200

    Added SourceRectangle property.

--- MonoGame.Framework.Extended/Drawable.cs
+++ MonoGame.Framework.Extended/Drawable.cs
@@ -15,6 +15,8 @@ namespace MonoGame.Framework.Extended
 
         public Texture2D Texture { get; set; }
 
+        public Rectangle? SourceRectangle { get; set; } = null;
+
         public Vector2 Position {
             get {
                 return PositionTransform.CurrentProgress;
@@ -60,37 +62,27 @@ namespace MonoGame.Framework.Extended
         }
 
         public Rectangle Bounds => new Rectangle(
-            (int)(Position.X + Size.X / 2), 
-            (int)(Position.Y + Size.Y / 2), 
+            (int)Position.X, 
+            (int)Position.Y, 
             (int)Size.X,  
             (int)Size.Y);
 
-        public Point Center => new Point(
-            Bounds.X + (int)Size.X / 2,
-            Bounds.Y + (int)Size.Y / 2);
-
         public Rectangle CenterRectangle(int width, int height) {
             return new Rectangle(
-                Center.X - width / 2, 
-                Center.Y - height / 2,
+                (int)Position.X - width / 2, 
+                (int)Position.Y - height / 2,
                 width, 
                 height);
         }
 
-        public bool CollidesWith(Drawable target) {
+        public bool IsCollidingWith(Drawable target) {
             return Bounds.Intersects(target.Bounds);
         }
 
-        public void MoveFacing(float amount) {
+        public void MoveFacingRotation(float amount) {
             Vector2 dir = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
-
             dir.Normalize();
-
             Position += dir * amount;
-        }
-
-        public Drawable() {
-
         }
 
         public Drawable(Texture2D texture, Vector2 size, Vector2 position, Color color) {
@@ -100,29 +92,13 @@ namespace MonoGame.Framework.Extended
             this.Color = color;
         }
 
-        public Drawable(Texture2D texture, Vector2 size, Vector2 position) {
-            this.Texture = texture;
-            this.Position = position;
-            this.Size = size;
+        public Drawable(Texture2D texture, Vector2 size, Vector2 position) : this(texture, size, position, Color.White) { }
 
-            this.Color = Color.White;
-        }
+        public Drawable(Texture2D texture, Vector2 size) : this(texture, size, Vector2.Zero, Color.White) { }
 
-        public Drawable(Texture2D texture, Vector2 size) {
-            this.Texture = texture;
-            this.Position = Vector2.Zero;
+        public Drawable(Texture2D texture) : this(texture, new Vector2(texture.Width, texture.Height), Vector2.Zero, Color.White) { }
 
-            this.Size = size;
-            this.Color = Color.White;
-        }
-
-        public Drawable(Texture2D texture) {
-            this.Texture = texture;
-
-            this.Position = Vector2.Zero;
-            this.Size = new Vector2(texture.Width, texture.Height);
-            this.Color = Color.White;
-        }
+        public Drawable() { }
 
         public virtual void Update(GameTime gameTime) {
             PositionTransform.Update(gameTime);
@@ -132,7 +108,7 @@ namespace MonoGame.Framework.Extended
         }
 
         public virtual void Draw(SpriteBatch spriteBatch) {
-            spriteBatch.Draw(Texture, Position, null, Color, Rotation, new Vector2(Texture.Width / 2f, Texture.Height / 2f), new Vector2(Size.X / Texture.Width, Size.Y / Texture.Height), SpriteEffect, Depth);
+            spriteBatch.Draw(Texture, Position, SourceRectangle, Color, Rotation, new Vector2(Texture.Width / 2f, Texture.Height / 2f), new Vector2(Size.X / Texture.Width, Size.Y / Texture.Height), SpriteEffect, Depth);
         }
     }
 }
